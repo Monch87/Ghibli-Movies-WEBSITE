76ad120dc1cc26e1a4c9cc16b37686ec5998bf4f
Barbara Diaz <barbarad91@gmail.com>
2021-02-13 14:59:20 +0100

    Add map with stores locations

---
+++ public/javascripts/stores-map-config.js
@@ -0,0 +1,44 @@
+var map;
+var service;
+var infowindow;
+
+function initMap() {
+  var sydney = new google.maps.LatLng(40.422682750576506, -3.7072195579467304);
+
+  infowindow = new google.maps.InfoWindow();
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: sydney,
+    zoom: 15
+  });
+
+  var request = {
+    query: "ghibli merchandising",
+    fields: ["name", "geometry", "place_id"],
+    location: sydney,
+    radius: 30
+  };
+
+  var service = new google.maps.places.PlacesService(map);
+
+  service.textSearch(request, function (results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      console.log(results);
+      for (var i = 0; i < results.length; i++) {
+        console.log(results[i].name);
+
+        createMarker(
+          results[i].geometry.location.lat(),
+          results[i].geometry.location.lng(),
+          results[i].name
+        );
+      }
+      //map.setCenter(results[0].geometry.location);
+    }
+  });
+}
+
+function createMarker(lat, lng, title) {
+  const position = { lat, lng };
+  new google.maps.Marker({ position, map, title });
+}

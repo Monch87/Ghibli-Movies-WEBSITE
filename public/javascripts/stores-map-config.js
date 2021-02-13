a688d38cd8439b5174ef550ff310cae49dc03d62
Barbara Diaz <barbarad91@gmail.com>
2021-02-13 17:55:46 +0100

    Refactor map positioning

--- public/javascripts/stores-map-config.js
+++ public/javascripts/stores-map-config.js
@@ -1,55 +1,52 @@
-var map;
-var service;
-var infowindow;
-
 function initMap() {
-  var sydney = new google.maps.LatLng(40.422682750576506, -3.7072195579467304);
-
-  infowindow = new google.maps.InfoWindow();
-
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: sydney,
-    zoom: 15
-  });
-
-  var request = {
-    query: "ghibli merchandising",
-    fields: ["name", "geometry", "place_id"],
-    location: sydney,
-    radius: 30
-  };
-
-  var service = new google.maps.places.PlacesService(map);
-
-  service.textSearch(request, function (results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      //console.log(results);
-      for (var i = 0; i < results.length; i++) {
-        //console.log(results[i].name);
-        getUserPosition(map);
-        createMarker(
-          results[i].geometry.location.lat(),
-          results[i].geometry.location.lng(),
-          results[i].name
-        );
-      }
-    }
-  });
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success);
+  }
 }
 
-function createMarker(lat, lng, title) {
+function createMarker(lat, lng, title, map) {
   const position = { lat, lng };
   new google.maps.Marker({ position, map, title });
 }
 
-function getUserPosition(map) {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const center = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      map.setCenter(center);
-    });
-  }
+function success(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+
+  const myLocation = new google.maps.LatLng(lat, lng);
+
+  const mapOptions = {
+    center: myLocation,
+    zoom: 11.5
+  };
+
+  const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+  searchPlaces(myLocation, map);
+}
+
+function searchPlaces(center, map) {
+  const request = {
+    query: "ghibli merchandising",
+    fields: ["name", "geometry", "place_id"],
+    location: center,
+    radius: 30
+  };
+
+  const service = new google.maps.places.PlacesService(map);
+
+  service.textSearch(request, (results, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      console.log(results);
+      for (let i = 0; i < results.length; i++) {
+        console.log(results[i].name);
+        createMarker(
+          results[i].geometry.location.lat(),
+          results[i].geometry.location.lng(),
+          results[i].name,
+          map
+        );
+      }
+    }
+  });
 }

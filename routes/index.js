9c80fa915c345676fd827f7d25c62b1d42fd1306
Barbara Diaz <barbarad91@gmail.com>
2021-02-16 18:38:46 +0100

    Solve conflict

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,5 @@ module.exports = (app) => {
   app.use("/stores", require("./stores.routes.js"));
   app.use("/api", require("./api.routes.js"));
   app.use("/profile", require("./profile.routes.js"));
-<<<<<<< HEAD
   app.use("/search", require("./search.routes.js"));
-=======
-  
-
-  // app.use('/ghibliapi', require('./ghibliapi.routes.js'))
-  // app.use('/googleplacesapi', require('./googleplacesapi.routes.js'))
-  // app.use('/googlemapsapi', require('./googlemapsapi.routes.js'))
-  // app.use('/search', require('./search.routes.js'))
->>>>>>> montse
 };
